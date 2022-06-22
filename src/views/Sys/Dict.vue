8bf8434a5a77ae561dddc05142f8d0bb0ced0d5d
WJH1393 <35983954+WJH1393@users.noreply.github.com>
2022-06-22 09:16:18 +0800

    修改系统管理业务数据展示

--- src/views/Sys/Dict.vue
+++ src/views/Sys/Dict.vue
@@ -109,7 +109,7 @@ export default {
 			if(data !== null) {
 				this.pageRequest = data.pageRequest
 			}
-			this.pageRequest.params = [{name:'label', value:this.filters.label}]
+			this.pageRequest.params = {name:this.filters.label}
 			this.$api.dict.findPage(this.pageRequest).then((res) => {
 				this.pageResult = res.data
 			}).then(data!=null?data.callback:'')
